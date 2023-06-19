18caed3f1477446bd6bb75f42b44bc730266b1ae
Jun <chchdelm3@icloud.com>
2023-06-20 08:06:14 +0900

    사용하지 않는 부분 주석처리

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -23,30 +23,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Investroberry'),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.lock_open_outlined), text: '로그인'),
-              Tab(icon: Icon(Icons.person), text: 'MY'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            LogIn(),
-            MyPage(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Investroberry'),
+//           bottom: TabBar(
+//             tabs: [
+//               Tab(icon: Icon(Icons.lock_open_outlined), text: '로그인'),
+//               Tab(icon: Icon(Icons.person), text: 'MY'),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             LogIn(),
+//             MyPage(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//
