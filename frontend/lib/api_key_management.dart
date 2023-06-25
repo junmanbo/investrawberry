1b79878dedbae208837894c239cf617c2e6c1f75
Jun <chchdelm3@icloud.com>
2023-06-26 07:38:42 +0900

    api key

--- frontend/lib/api_key_management.dart
+++ frontend/lib/api_key_management.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'api_service.dart';
+import 'login_state_manager.dart';
 
 class ApiKeyManagement extends StatelessWidget {
+  final ApiService _apiService = ApiService();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Replace this with your actual data
-    final apiKeys = [
-      {'exchange': 'Binance', 'accessKey': '123456'},
-      {'exchange': 'Coinbase', 'accessKey': '789012'},
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: Text('API Key 관리'),
@@ -16,29 +15,42 @@ class ApiKeyManagement extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         centerTitle: true,
       ),
-      body: apiKeys.isEmpty
-          ? Center(
-              child: Text(
-                '등록된 API Key 가 없습니다. 아래의 + 버튼을 눌러 Key를 추가해 주세요.',
-              ),
-            )
-          : ListView.builder(
-              itemCount: apiKeys.length,
-              itemBuilder: (context, index) {
-                final apiKey = apiKeys[index];
-                return ListTile(
-                  leading: Image.asset('assets/images/logo.png'), // Replace with your actual logo image
-                  title: Text(apiKey['exchange'] ?? 'Unknown'),
-                  subtitle: Text(apiKey['accessKey'] ?? 'Unknown'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      // TODO: Implement delete functionality
-                    },
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _apiService.getApiKeys(Provider.of<LoginStateManager>(context).jwtToken),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // Show loading spinner while waiting for data
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}'); // Show error message if there's an error
+          } else {
+            final apiKeys = snapshot.data ?? [];
+            return apiKeys.isEmpty
+              ? Center(
+                  child: Text(
+                    '등록된 API Key 가 없습니다. 아래의 + 버튼을 눌러 Key를 추가해 주세요.',
                   ),
+                )
+              : ListView.builder(
+                  itemCount: apiKeys.length,
+                  itemBuilder: (context, index) {
+                    final apiKey = apiKeys[index];
+                    return ListTile(
+                      leading: Image.asset('assets/images/logo.png'), // Replace with your actual logo image
+                      title: Text(apiKey['exchange']['exchange_nm'] ?? 'Unknown'),
+                      //subtitle: Text(apiKey['access_key'] ?? 'Unknown'),
+                      subtitle: Text(apiKey['access_key']?.substring(0, 10) ?? 'Unknown'),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          // TODO: Implement delete functionality
+                        },
+                      ),
+                    );
+                  },
                 );
-              },
-            ),
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: Colors.redAccent, // Same color as AppBar
--- frontend/lib/api_service.dart
+++ frontend/lib/api_service.dart
@@ -5,66 +5,74 @@ class ApiService {
   static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
 
   Future<String> login(String email, String password) async {
-    try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/login/access-token'),
-        body: {'username': email, 'password': password},
-      );
-
-      if (response.statusCode == 200) {
-        final jsonResponse = jsonDecode(response.body);
-        final accessToken = jsonResponse['access_token'];
-        return accessToken;
-      } else {
-        throw Exception('Failed to log in.');
-      }
-    } catch (e) {
-      throw Exception(e.toString());
-    }
+    final response = await _post('$baseUrl/login/access-token', {'username': email, 'password': password});
+    return response['access_token'];
   }
 
   Future<Map<String, dynamic>> getUserInfo(String token) async {
+    final response = await _get('$baseUrl/users/me', {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'});
+    return response;
+  }
+
+  Future<bool> signUp(String email, String password, String fullname) async {
+    final response = await _post(
+      '$baseUrl/users/open',
+      {'email': email, 'password': password, 'full_name': fullname},
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response != null) {
+      print('회원가입 완료');
+      return true;
+    } else {
+      print('회원가입 실패, 응답 본문: ${response.toString()}'); // 응답 본문 출력
+      return false;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getApiKeys(String token) async {
     try {
       final response = await http.get(
-        Uri.parse('$baseUrl/users/me'),
+        Uri.parse('$baseUrl/exchangekeys'),
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $token',
         },
       );
 
-      if (response.statusCode == 200) {
-        final jsonResponse = jsonDecode(response.body);
-        return jsonResponse;
-      } else {
-        throw Exception('Failed to load user info.');
-      }
+      _checkStatusCode(response.statusCode);
+
+      return List<Map<String, dynamic>>.from(jsonDecode(response.body));
     } catch (e) {
-      throw Exception(e.toString());
+      throw Exception('Failed to load API keys: $e');
     }
   }
 
-  Future<bool> signUp(String email, String password, String fullname) async {
+  // method
+  Future<Map<String, dynamic>> _get(String url, Map<String, String> headers) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/users/open'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({
-          'email': email, 
-          'password': password, 
-          'full_name': fullname
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        print('회원가입 완료');
-        return true;
-      } else {
-        print('회원가입 실패, 응답 본문: ${response.body}'); // 응답 본문 출력
-        return false;
-      }
+      final response = await http.get(Uri.parse(url), headers: headers);
+      _checkStatusCode(response.statusCode);
+      return jsonDecode(response.body);
     } catch (e) {
-      throw Exception(e.toString());
+      throw Exception('Failed to GET $url: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>> _post(String url, Map<String, dynamic> body, {Map<String, String> headers = const {}}) async {
+    try {
+      //final response = await http.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+      final response = await http.post(Uri.parse(url), headers: headers, body: url.contains('/login/access-token') ? body : jsonEncode(body));
+      _checkStatusCode(response.statusCode);
+      return jsonDecode(response.body);
+    } catch (e) {
+      throw Exception('Failed to POST $url: $e');
+    }
+  }
+
+  void _checkStatusCode(int statusCode) {
+    if (statusCode < 200 || statusCode >= 300) {
+      throw Exception('Failed with status code: $statusCode');
     }
   }
 }
