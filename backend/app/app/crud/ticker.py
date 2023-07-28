0d3c789d61e6ccc735aacccab89ea6363e0aed12
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-07-28 09:30:50 +0900

    가격 붙이고 호출제한 피하기 위해 개수 줄임

--- backend/app/app/crud/ticker.py
+++ backend/app/app/crud/ticker.py
@@ -8,7 +8,7 @@ from app.schemas.ticker import TickerCreate, TickerUpdate
 
 
 class CRUDTicker(CRUDBase[Ticker, TickerCreate, TickerUpdate]):
-    def search_ticker_by_query(self, db: Session, *, query: str, skip: int = 0, limit: int = 10) -> List[Ticker]:
+    def search_ticker_by_query(self, db: Session, *, query: str, skip: int = 0, limit: int = 5) -> List[Ticker]:
         return db.query(Ticker).filter(Ticker.ticker_knm.ilike(f"%{query}%")).offset(skip).limit(limit).all()
 
 
