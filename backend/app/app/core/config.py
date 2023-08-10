8f5584a14279eeddcd3fd2eb9d5e8e705fbd1a53
Byeong Jun <chchdelm3@icloud.com>
2023-08-11 00:34:13 +0900

    checking type

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -1,13 +1,12 @@
 from dotenv import find_dotenv, load_dotenv
-
-load_dotenv(find_dotenv(usecwd=True))
-
 import os
 import secrets
 from typing import Any, Dict, List, Optional
 
-from pydantic import AnyHttpUrl, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import AnyHttpUrl, EmailStr, PostgresDsn, validator
 from pydantic_settings import BaseSettings
+
+load_dotenv(find_dotenv(usecwd=True))
 
 
 class Settings(BaseSettings):
@@ -15,9 +14,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 1 days = 1 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 1
-    SERVER_NAME: str | None = os.getenv("SERVER_NAME")
-    SERVER_HOST: AnyHttpUrl | None = os.getenv("SERVER_HOST")
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+    SERVER_NAME: str | None = os.getenv("SERVER_NAME", "")
+    SERVER_HOST: str | AnyHttpUrl = os.getenv("SERVER_HOST", "http://localhost:8000")
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List = [
         "http://localhost:3000",
         "http://localhost",
     ]
@@ -30,15 +29,7 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    PROJECT_NAME: str | None = os.getenv("PROJECT_NAME")
-    # SENTRY_DSN: Optional[HttpUrl] = None
-
-    # @validator("SENTRY_DSN", pre=True)
-    # def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
-    #    if len(v) == 0:
-    #        return None
-    #    return v
-
+    PROJECT_NAME: str = os.getenv("PROJECT_NAME", "MyApp")
     POSTGRES_SERVER: str | None = os.getenv("POSTGRES_SERVER")
     POSTGRES_USER: str | None = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str | None = os.getenv("POSTGRES_PASSWORD")
@@ -57,34 +48,7 @@ class Settings(BaseSettings):
             path=values.get("POSTGRES_DB") or "",
         )
 
-    SMTP_TLS: bool = False
-    SMTP_PORT: Optional[int] = None
-    SMTP_HOST: Optional[str] = None
-    SMTP_USER: Optional[str] = None
-    SMTP_PASSWORD: Optional[str] = None
-    EMAILS_FROM_EMAIL: Optional[EmailStr] = None
-    EMAILS_FROM_NAME: Optional[str] = None
-
-    @validator("EMAILS_FROM_NAME")
-    def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
-        if not v:
-            return values["PROJECT_NAME"]
-        return v
-
-    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
-    EMAILS_ENABLED: bool = False
-
-    @validator("EMAILS_ENABLED", pre=True)
-    def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
-        return bool(
-            values.get("SMTP_HOST")
-            and values.get("SMTP_PORT")
-            and values.get("EMAILS_FROM_EMAIL")
-        )
-
-    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
-    FIRST_SUPERUSER: EmailStr = os.getenv("FIRST_SUPERUSER")
+    FIRST_SUPERUSER: EmailStr | str = os.getenv("FIRST_SUPERUSER", "admin")
     FIRST_SUPERUSER_PASSWORD: str | None = os.getenv("FIRST_SUPERUSER_PASSWORD")
     FIRST_NAME: str | None = os.getenv("FIRST_NAME")
     USERS_OPEN_REGISTRATION: bool = True
