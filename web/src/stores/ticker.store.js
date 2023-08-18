66b26a66b4b2ab38997c384088729c63519113c6
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-18 13:46:23 +0900

    검색 티커 초기화

--- web/src/stores/ticker.store.js
+++ web/src/stores/ticker.store.js
@@ -5,36 +5,39 @@ import { fetchWrapper } from '@/helpers';
 const baseUrl = `${import.meta.env.VITE_API_URL}/ticker`;
 
 export const useTickerStore = defineStore({
-    id: 'ticker',
-    state: () => ({
-        searchResult: [],
-        topTickers: [], // 시가총액 상위 3개의 종목을 저장하는 상태 추가
-        selectedTicker: null
-    }),
-    actions: {
-        async searchTicker(query) {
-            if (query) {
-                try {
-                    const response = await fetchWrapper.get(`${baseUrl}?keyword=${query}`);
-                    this.searchResult = response;
-                } catch (error) {
-                    console.error(`Error searching for ${query}:`, error);
-                }
-            } else {
-                this.searchResult = [];
-            }
-        },
-        async getTopTickers() { // 시가총액 상위 3개의 종목을 조회하는 액션 추가
-            try {
-                const response = await fetchWrapper.get(`${baseUrl}/top`);
-                this.topTickers = response;
-            } catch (error) {
-                console.error('Error getting top tickers:', error);
-            }
-        },
-        selectTicker(ticker) {
-            this.selectedTicker = ticker;
+  id: 'ticker',
+  state: () => ({
+    searchResult: [],
+    topTickers: [], // 시가총액 상위 3개의 종목을 저장하는 상태 추가
+    selectedTicker: null
+  }),
+  actions: {
+    async searchTicker(query) {
+      if (query) {
+        try {
+          const response = await fetchWrapper.get(`${baseUrl}?keyword=${query}`);
+          this.searchResult = response;
+        } catch (error) {
+          console.error(`Error searching for ${query}:`, error);
         }
+      } else {
+        this.searchResult = [];
+      }
+    },
+    async getTopTickers() { // 시가총액 상위 3개의 종목을 조회하는 액션 추가
+      try {
+        const response = await fetchWrapper.get(`${baseUrl}/top`);
+        this.topTickers = response;
+      } catch (error) {
+        console.error('Error getting top tickers:', error);
+      }
+    },
+    selectTicker(ticker) {
+      this.selectedTicker = ticker;
+    },
+    clearSearchResult() { // clearSearchResult 함수를 추가합니다.
+      this.searchResult = [];
     }
+  }
 });
 
