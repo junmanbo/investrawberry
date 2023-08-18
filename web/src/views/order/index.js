8b7aefcaeb58a3a595f24348692ae127000c4707
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-18 16:13:47 +0900

    portfolio 종목 추가 및 가져오기 구현

--- web/src/views/order/index.js
+++ web/src/views/order/index.js
@@ -3,3 +3,5 @@ export { default as Order } from './Order.vue';
 export { default as Portfolio } from './Portfolio.vue';
 export { default as OrderMenu } from './OrderMenu.vue';
 export { default as Search } from './Search.vue';
+export { default as SearchModal } from './SearchModal.vue';
+export { default as PortfolioModal } from './PortfolioModal.vue';
