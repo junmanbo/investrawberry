774d8f13400da0ffab2806d4a0a15acf77dba5fe
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-21 10:34:43 +0900

    exchange_class 가져와서 사용

--- backend/app/app/api/api_v1/endpoints/balance.py
+++ backend/app/app/api/api_v1/endpoints/balance.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from app import crud, models
 from app.api import deps
-from app.trading import upbit, kis
+from app import EXCHANGE_CLASSES
 
 router = APIRouter()
 
@@ -21,12 +21,15 @@ async def get_balance_all(
     exchange_keys = crud.exchange_key.get_multi_by_owner(db, owner_id=current_user.id)
     total_balance = {}
     for key in exchange_keys:
-        if key.exchange.exchange_nm == "UPBIT":
-            client = upbit.Upbit(key.access_key, key.secret_key)
-        elif key.exchange.exchange_nm == "KIS":
-            client = kis.KIS(key.access_key, key.secret_key, key.account)
+        exchange_nm = key.exchange.exchange_nm
+        if exchange_nm in EXCHANGE_CLASSES:
+            client_class = EXCHANGE_CLASSES[exchange_nm]
+            client = client_class(key.access_key, key.secret_key, key.account)
+            balance = client.get_total_balance()
+            total_balance[exchange_nm] = balance
         else:
-            raise HTTPException(status_code=400, detail="Exchange is not found.")
+            raise HTTPException(status_code=404, detail="Exchange is not found.")
+
         balance = client.get_total_balance()
         total_balance[key.exchange.exchange_nm] = balance
 
