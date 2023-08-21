f379bb36f4be82308d8544c11857516c2ff3e3ef
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-21 10:38:45 +0900

    portfolio order celery 작업 추가

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -3,14 +3,14 @@ from celery import Celery
 
 # celery_app = Celery("worker", broker="amqp://guest@queue//")
 celery_app = Celery(
-    "worker", 
-    broker="amqp://guest@localhost:5672//", 
-    backend="redis://localhost:6379/0"
+    "worker", broker="amqp://guest@localhost:5672//", backend="redis://localhost:6379/0"
 )
 celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.task_routes = {
-    "app.worker.place_order": "order"
+    "app.worker.place_order": "order",
+    "app.worker.portfolio_order": "order",
 }
+
 
 def get_task_info(task_id):
     """
@@ -23,4 +23,3 @@ def get_task_info(task_id):
         "task_result": task_result.result,
     }
     return result
-
