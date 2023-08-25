6603df1131001dfa2d7bd52089719bcd38091b76
Jun <chchdelm3@icloud.com>
2023-08-25 16:32:54 +0900

    cors 등록

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List = [
         "http://localhost:3000",
         "http://localhost",
-        "http://localhost:80",
+        "http://frontend",
     ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
