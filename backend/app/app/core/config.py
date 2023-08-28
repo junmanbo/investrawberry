6ee31365bdd766264b3fb1f597ee061a750d2206
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-29 08:31:09 +0900

    shrink the expire time of token

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -12,8 +12,8 @@ load_dotenv(find_dotenv(usecwd=True))
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    # 60 minutes * 24 hours * 1 days = 1 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 1
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 12
     SERVER_NAME: str | None = os.getenv("SERVER_NAME", "")
     SERVER_HOST: str | AnyHttpUrl = os.getenv("SERVER_HOST", "http://localhost:8000")
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List = [
