76dfa43bb1cee847f26b0070be381344f5db0e50
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-28 15:09:26 +0900

    token cookie 로 전달

--- backend/app/app/api/api_v1/endpoints/login.py
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import Any
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -15,7 +15,9 @@ router = APIRouter()
 
 @router.post("/login/access-token", response_model=schemas.Token)
 async def login_access_token(
-    db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
+    db: Session = Depends(deps.get_db),
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    response: Response = Depends(),
 ) -> Any:
     """
     OAuth2 compatible token login, get an access token for future requests
@@ -30,6 +32,13 @@ async def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = security.create_access_token(
         user.id, expires_delta=access_token_expires
+    )
+    # JWT 토큰을 쿠키로 설정하여 응답 헤더에 추가한다.
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=False,  # HTTPS 연결만 가능하도록 설정 (개발 환경에서는 False)
     )
     return {
         "access_token": access_token,
