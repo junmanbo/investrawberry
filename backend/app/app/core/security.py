8ef79cbb616a09ef6ae72728826b1d2ef20329d2
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-29 13:15:06 +0900

    날짜 계산해서 날짜만 받아서 저장 & 용도 표시

--- backend/app/app/core/security.py
+++ backend/app/app/core/security.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Any
 
 from jose import jwt
@@ -12,16 +12,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
-def create_access_token(
-    subject: str | Any, expires_delta: timedelta | None = None
-) -> str:
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode = {"exp": expire, "sub": str(subject)}
+def create_token(subject: str | Any, expire: datetime, usage: str) -> str:
+    to_encode = {"exp": expire, "sub": str(subject), "usage": usage}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
