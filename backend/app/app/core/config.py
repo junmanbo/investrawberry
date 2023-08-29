5037ee78f2b66dc76dcb09bcbd937278b8dd59be
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-29 13:14:24 +0900

    토큰 만료 시간 변경

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 12
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     SERVER_NAME: str | None = os.getenv("SERVER_NAME", "")
     SERVER_HOST: str | AnyHttpUrl = os.getenv("SERVER_HOST", "http://localhost:8000")
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List = [
