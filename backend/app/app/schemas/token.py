1608241a62b8a4376ab77be0e777c11b3c533ce4
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-30 15:47:36 +0900

    refresh token 도 제외

--- backend/app/app/schemas/token.py
+++ backend/app/app/schemas/token.py
@@ -3,11 +3,8 @@ from pydantic import BaseModel
 
 class Token(BaseModel):
     access_token: str
-    refresh_token: str | None = None
     token_type: str
-    user_name: str | None = None
 
 
 class TokenPayload(BaseModel):
     sub: int
-    usage: str
