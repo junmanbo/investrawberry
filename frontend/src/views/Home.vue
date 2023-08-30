b93a4710f812de117bae83ad969edb555a7f2ea1
Byeongjun Kim <byeongjun.kim@happyblock.io>
2023-08-30 09:37:58 +0900

    change font size

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div v-if="user">
         <div class="jumbotron text-center">
-            <h1 class="display-8">{{user.user_name}}님, 안녕하세요!</h1>
-            <p class="lead">오늘도 투자 성공하세요 :)</p>
+            <h1 class="display-8 fs-4">{{user.user_name}}님, 안녕하세요!</h1>
+            <p class="lead fs-6">오늘도 투자 성공하세요 :)</p>
         </div>
         <div class="container">
             <div class="row">
