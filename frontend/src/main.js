1cd2a45aa9e81499e0ac59c6b61d99baa43f8bb6
Byeong Jun <chchdelm3@icloud.com>
2023-09-09 17:50:53 +0900

    back to previous createApp

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,33 +10,3 @@ app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
-
-
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
-import App from './App.vue';
-import { router } from './helpers';
-import { useAuthStore } from './stores';
-
-startApp();
-
-// async start function to enable waiting for refresh token call
-async function startApp () {
-    const app = createApp(App);
-
-    app.use(createPinia());
-    app.use(router);
-
-    // attempt to auto refresh token before startup
-    try {
-        const authStore = useAuthStore();
-        await authStore.refreshAccessToken();
-    } catch (error) {
-        // catch error to start app on success or failure
-        console.log(error);
-    }
-
-    app.mount('#app');
-}
