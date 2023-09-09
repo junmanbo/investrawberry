1d12d0ebec615b23bd7d7b563569d2b7ba6adb25
Byeong Jun <chchdelm3@icloud.com>
2023-09-09 18:58:11 +0900

    cors 등록

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -20,8 +20,8 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] | List = [
         "http://localhost:3000",
         "http://localhost",
-        "http://duckdns.org",
-        "http://backend.duckdns.org",
+        "http://tutong.kr",
+        "https://tutong.kr",
     ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
