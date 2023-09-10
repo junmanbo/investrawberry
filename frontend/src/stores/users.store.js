03d83eaba35d9bc6a5020506ea2f505cde5b7964
Byeong Jun <chchdelm3@icloud.com>
2023-09-10 22:25:55 +0900

    user 등록만 실행

--- frontend/src/stores/users.store.js
+++ frontend/src/stores/users.store.js
@@ -8,57 +8,17 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/users`;
 export const useUsersStore = defineStore({
     id: 'users',
     state: () => ({
-        users: {},
-        user: {}
+        user: JSON.parse(sessionStorage.getItem('user')),
     }),
     actions: {
         async register(user) {
             await fetchWrapper.post(`${baseUrl}/open`, user);
         },
-        async getAll() {
-            this.users = { loading: true };
-            try {
-                this.users = await fetchWrapper.get(baseUrl);    
-            } catch (error) {
-                this.users = { error };
-            }
-        },
-        async getById(id) {
-            this.user = { loading: true };
-            try {
-                this.user = await fetchWrapper.get(`${baseUrl}/${id}`);
-            } catch (error) {
-                this.user = { error };
-            }
-        },
-        async update(id, params) {
-            await fetchWrapper.put(`${baseUrl}/${id}`, params);
-
-            // update stored user if the logged in user updated their own record
-            const authStore = useAuthStore();
-            if (id === authStore.user.id) {
-                // update local storage
-                const user = { ...authStore.user, ...params };
-                localStorage.setItem('user', JSON.stringify(user));
-
-                // update auth user in pinia state
-                authStore.user = user;
-            }
-        },
-        async delete(id) {
-            // add isDeleting prop to user being deleted
-            this.users.find(x => x.id === id).isDeleting = true;
-
-            await fetchWrapper.delete(`${baseUrl}/${id}`);
-
-            // remove user from list after deleted
-            this.users = this.users.filter(x => x.id !== id);
-
-            // auto logout if the logged in user deleted their own record
-            const authStore = useAuthStore();
-            if (id === authStore.user.id) {
-                authStore.logout();
-            }
+        async getUserInfo() {
+            const userInfo = await fetchWrapper.get(`${baseUrl}/me`);
+            this.user = {...this.user, user_name: userInfo.full_name};
+            sessionStorage.setItem('user', JSON.stringify(this.user));
         }
     }
 });
+
