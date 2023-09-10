c12a298512e9820d852624a185c457a5749b9800
Byeong Jun <chchdelm3@icloud.com>
2023-09-10 10:22:17 +0900

    초기 데이터 생성만 실행

--- backend/app/initial_script.sh
+++ backend/app/initial_script.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Create table
-mkdir alembic/versions
-alembic upgrade head
-alembic revision --autogenerate
-alembic upgrade head
-
 # DB 체크
 python3 app/initial_data.py
 python3 app/backend_pre_start.py
-
-# DB 데이터 생성
-#python3 input/ticker.py
-
